7dc87fdcd4d0253fae26827c3b093ed6a665eb69
Qovop-Dev <sharif.elsayed@outlook.fr>
2024-08-21 20:39:55 +0200

    add full word shuffle function + add word reverse function + some new tests

--- shakestr.go
+++ shakestr.go
@@ -2,26 +2,56 @@ package shakestr
 
 import (
 	"fmt"
-	"math/rand/v2"
 	"strings"
+	"time"
+
+	"golang.org/x/exp/rand"
 )
 
 const (
-	WordParam = "w"
-	TextParam = "t"
+	wordParam    = "w"
+	textParam    = "t"
+	middleParam  = "m"
+	fullParam    = "f"
+	reverseParam = "r"
 )
 
-// input s = word or text to shake
-// input p = parameter (w for word, t for text)
+// input s = the word or text to shuffle
+// input p = parameter ( -> 1st letter = input type : w(word), t(text)
+//
+//	-> after 1st letter = shake methode :
+//	          m(shuffle middle of word),
+//	          f(shuffle the full word),
+//	          r(reverse the entire word))
 func Shake(s string, p string) (string, error) {
 
-	switch strings.ToLower(p) {
-	// word to be shake
-	case WordParam:
-		return shakeWord(s)
+	// Check the input is not empty
+	if s == "" {
+		return "", fmt.Errorf("input value not valid, cannot be empty")
+	}
 
-	// text to be shake
-	/*case TextParam:
+	// the parameter value should be at least 2 caracters
+	if len(p) < 2 {
+		return "", fmt.Errorf("parameter length must be at least 2, got: %s", p)
+	}
+
+	// Assign the parameters
+	inputType, shakeMethod := strings.ToLower(string(p[0])), strings.ToLower(string(p[1]))
+
+	switch inputType {
+	// Action on single word
+	case wordParam:
+		switch shakeMethod {
+		case middleParam, fullParam:
+			return shakeWord(s, shakeMethod)
+		case reverseParam:
+			return reverse(s)
+		default:
+			return "", fmt.Errorf("unknown parameter: %s", p)
+		}
+
+	// Action on text
+	/*case textParam:
 
 	 */
 	// unknown parameter
@@ -31,49 +61,63 @@ func Shake(s string, p string) (string, error) {
 
 }
 
-func shakeWord(s string) (string, error) {
+func shakeWord(s string, p string) (string, error) {
 	// convert to rune
 	runes := []rune(strings.TrimSpace(s))
 
-	// check word lenght
-	if len(runes) <= 3 { // Si le mot est trop court, pas besoin de mélanger
-		return "", fmt.Errorf("the word length must be greater than 3")
-	}
+	// buffer variable to work on
+	bufferRunes := make([]rune, len(runes))
+	copy(bufferRunes, runes)
 
 	// Check if it contains any spaces
 	if strings.Contains(string(runes), " ") {
 		return "", fmt.Errorf("input must be a single word without spaces, got: %s", s)
 	}
 
-	// Shake runes between first and last letter
-	middle := runes[1 : len(runes)-1]
+	// check word length
+	if p == middleParam {
+		if len(runes) <= 3 {
+			return string(runes), nil
+		}
+		// Shake runes between first and last letter
+		bufferRunes = runes[1 : len(runes)-1]
+	} else {
+		if len(runes) == 1 {
+			return string(runes), nil
+		}
+	}
 
-	//check middle has different letters
+	//check bufferRunes has different letters
 	valid := false
-	for i := 0; i < len(middle)-1; i++ {
-		if middle[i] != middle[i+1] {
+	for i := 0; i < len(bufferRunes)-1; i++ {
+		if bufferRunes[i] != bufferRunes[i+1] {
 			valid = true
 			break
 		}
 	}
 	if !valid {
-		return "", fmt.Errorf("the word cannot be shuffled because it contains identical adjacent letters in the middle")
+		if p == middleParam {
+			return "", fmt.Errorf("the word cannot be shuffled because it contains identical adjacent letters in the middle")
+		} else {
+			return "", fmt.Errorf("the word cannot be shuffled because it contains identical letters")
+		}
 	}
 
-	// Copy middle ton check the rand.shuffle result
-	original := make([]rune, len(middle))
-	copy(original, middle)
+	// Copy middle to check the rand.shuffle result
+	original := make([]rune, len(bufferRunes))
+	copy(original, bufferRunes)
 
 	// Loop until the shuffle changes the order of the middle letters
 	for {
-		rand.Shuffle(len(middle), func(i, j int) {
-			middle[i], middle[j] = middle[j], middle[i]
+		rand.Seed(uint64(time.Now().UnixNano()))
+		rand.Shuffle(len(bufferRunes), func(i, j int) {
+			bufferRunes[i], bufferRunes[j] = bufferRunes[j], bufferRunes[i]
 		})
 
-		// Check if middle is different from original
+		// Check if bufferRunes is different from original
 		changed := false
-		for i := range middle {
-			if middle[i] != original[i] {
+		for i := range bufferRunes {
+			if bufferRunes[i] != original[i] {
 				changed = true
 				break
 			}
@@ -86,7 +130,30 @@ func shakeWord(s string) (string, error) {
 	}
 
 	// Rebuild the word
-	shaked := string(runes[0]) + string(middle) + string(runes[len(runes)-1])
+	var shaked string
+	if p == middleParam {
+		shaked = string(runes[0]) + string(bufferRunes) + string(runes[len(runes)-1])
+	} else {
+		shaked = string(bufferRunes)
+	}
 
 	return shaked, nil
+}
+
+func reverse(s string) (string, error) {
+	// convert to rune
+	runes := []rune(strings.TrimSpace(s))
+	reverseRunes := make([]rune, len(runes))
+
+	// check word length
+	if len(runes) == 1 {
+		return string(runes), nil
+	}
+
+	// reverse the word
+	for i := range runes {
+		reverseRunes[len(runes)-i-1] = runes[i]
+	}
+
+	return string(reverseRunes), nil
 }
--- shakestr_test.go
+++ shakestr_test.go
@@ -11,14 +11,32 @@ func TestShakeforWord(t *testing.T) {
 		shaked string
 		errMsg string
 	}{
-		{"Ace", "w", "", "the word length must be greater than 3"},
-		{"Go", "w", "", "the word length must be greater than 3"},
-		{"Test", "w", "Tset", ""},
-		{"Test", "W", "Tset", ""},
-		{" Test ", "W", "Tset", ""},
-		{"Boot", "W", "", "the word cannot be shuffled because it contains identical adjacent letters in the middle"},
-		{"Test", "Z", "", "unknown parameter: Z"},
-		{"Invalid Test", "w", "", "input must be a single word without spaces, got: Invalid Test"},
+		{"", "wm", "", "input value not valid, cannot be empty"},
+		{"Go", "w", "", "parameter length must be at least 2, got: w"},
+		//shuffle middle of the word
+		{"Ace", "wm", "Ace", ""},
+		{"Go", "wm", "Go", ""},
+		{"G", "wm", "G", ""},
+		{"Test", "wm", "Tset", ""},
+		{"Test", "Wm", "Tset", ""},
+		{" Test ", "Wm", "Tset", ""},
+		{"Boot", "Wm", "", "the word cannot be shuffled because it contains identical adjacent letters in the middle"},
+		{"Test", "Zm", "", "unknown parameter: Zm"},
+		{"Test", "wZ", "", "unknown parameter: wZ"},
+		{"Invalid Test", "wm", "", "input must be a single word without spaces, got: Invalid Test"},
+		//shuffle the full word
+		{"G", "wf", "G", ""},
+		{"Go", "wf", "oG", ""},
+		{"Go", "Wf", "oG", ""},
+		{" Go ", "Wf", "oG", ""},
+		{"oo", "Wf", "", "the word cannot be shuffled because it contains identical letters"},
+		{"Go", "Zf", "", "unknown parameter: Zf"},
+		{"Go", "wZ", "", "unknown parameter: wZ"},
+		{"Invalid Test", "wf", "", "input must be a single word without spaces, got: Invalid Test"},
+		//reverse the full word
+		{"A", "wr", "A", ""},
+		{"Ace", "wr", "ecA", ""},
+		{"Hello You!", "wr", "!uoY olleH", ""},
 	}
 
 	for _, tt := range tests {
