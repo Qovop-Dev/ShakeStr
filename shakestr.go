d50afdfc3ced3781d9b425b5e0894d522e89d2c3
Qovop-Dev <sharif.elsayed@outlook.fr>
2024-08-23 23:59:57 +0200

    add shake text function

--- shakestr.go
+++ shakestr.go
@@ -3,7 +3,9 @@ package shakestr
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
+	"unicode"
 
 	"golang.org/x/exp/rand"
 )
@@ -14,51 +16,154 @@ const (
 	middleParam  = "m"
 	fullParam    = "f"
 	reverseParam = "r"
+	placeParam   = "p"
 )
 
+//-----------------------------------------------------------------------//
+//-----------------------------------------------------------------------//
 // input s = the word or text to shuffle
 // input p = parameter ( -> 1st letter = input type : w(word), t(text)
 //
-//	-> after 1st letter = shake methode :
+//	-> after 1st letter for word type input = shake method :
 //	          m(shuffle middle of word),
 //	          f(shuffle the full word),
 //	          r(reverse the entire word))
-func Shake(s string, p string) (string, error) {
 
-	// Check the input is not empty
-	if s == "" {
-		return "", fmt.Errorf("input value not valid, cannot be empty")
-	}
+//	-> after 1st letter for text type input = shake method :
+//	          r(reverse the whole text),
+//	          rp(reverse only the place of words in the text),
+//	          f(shuffle every word (entire) but keep the order)
+//	          fp(shuffle every word (entire) and the order),
+//	          m(shuffle every word (middle) but keep the order),
+//	          mp(shuffle every word (middle) and the order),
+//	          p(shuffle only the order))
+//-----------------------------------------------------------------------//
+//-----------------------------------------------------------------------//
 
-	// the parameter value should be at least 2 caracters
-	if len(p) < 2 {
-		return "", fmt.Errorf("parameter length must be at least 2, got: %s", p)
+func Shake(s, p string) (string, error) {
+
+	//Validate Inputs
+	if err := validateInputs(s, p); err != nil {
+		return "", err
 	}
 
 	// Assign the parameters
-	inputType, shakeMethod := strings.ToLower(string(p[0])), strings.ToLower(string(p[1]))
+	inputType, shakeMethod, shakeMethodOpt := parseParameters(p)
 
 	switch inputType {
-	// Action on single word
 	case wordParam:
-		switch shakeMethod {
-		case middleParam, fullParam:
-			return shakeWord(s, shakeMethod)
-		case reverseParam:
-			return reverse(s)
-		default:
-			return "", fmt.Errorf("unknown parameter: %s", p)
-		}
+		// Action on single word
+		return wordProcess(s, shakeMethod)
 
-	// Action on text
-	/*case textParam:
+	case textParam:
+		// Action on text
+		return textProcess(s, shakeMethod, shakeMethodOpt)
 
-	 */
-	// unknown parameter
 	default:
+		// unknown parameter
 		return "", fmt.Errorf("unknown parameter: %s", p)
 	}
 
+}
+
+//-----------------------------------------//
+//---------  Principal Functions  ---------//
+//-----------------------------------------//
+
+func validateInputs(s, p string) error {
+	// Check the input is not empty
+	if s == "" {
+		return fmt.Errorf("input value not valid, cannot be empty")
+	}
+
+	// the parameter value should be between 2 and 3 caracters
+	if len(p) < 2 || len(p) > 3 {
+		return fmt.Errorf("parameter length is not valid, got: %s", p)
+	}
+
+	return nil
+}
+
+func parseParameters(p string) (inputType, shakeMethod, shakeMethodOpt string) {
+	inputType = strings.ToLower(string(p[0]))
+	shakeMethod = strings.ToLower(string(p[1]))
+	if len(p) == 3 {
+		shakeMethodOpt = strings.ToLower(string(p[2]))
+	}
+	return
+}
+
+func wordProcess(s, shakeMethod string) (string, error) {
+	switch shakeMethod {
+	case middleParam, fullParam:
+		return shakeWord(s, shakeMethod)
+	case reverseParam:
+		return reverse(s)
+	default:
+		return "", fmt.Errorf("unknown parameter: w%s", shakeMethod)
+	}
+}
+
+func textProcess(s, shakeMethod, shakeMethodOpt string) (string, error) {
+	if shakeMethod == reverseParam && shakeMethodOpt == "" {
+		return reverse(s)
+	} else {
+		return shakeText(s, shakeMethod, shakeMethodOpt)
+	}
+}
+
+//-----------------------------------------//
+//--------- Operational Functions ---------//
+//-----------------------------------------//
+
+func shakeText(s string, shakeMethod string, shakeMethodOpt string) (string, error) {
+	// cut text in slice of words
+	words := []string(strings.Fields(strings.TrimSpace(s)))
+
+	// check words slice length
+	if len(words) == 1 {
+		return "", fmt.Errorf("input must be more than one word, got: %s", strings.Join(words, ""))
+	}
+
+	switch shakeMethod {
+
+	case middleParam, fullParam:
+		shakedWords := make([]string, len(words))
+		// init waitgroup
+		var wg sync.WaitGroup
+
+		// launch every word in go routine
+		for i := range words {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				shakedWord, _ := shakeWord(words[i], shakeMethod)
+				shakedWords[i] = shakedWord
+			}(i)
+		}
+		// wait the end of process
+		wg.Wait()
+
+		//shake place of word is asked
+		if shakeMethodOpt == placeParam {
+			return shakePlace(shakedWords)
+		} else {
+			return strings.Join((shakedWords), " "), nil
+		}
+
+	case placeParam:
+		return shakePlace(words)
+
+	case reverseParam:
+		if shakeMethodOpt == placeParam {
+			return reversePlace(words)
+		} else {
+			return "", fmt.Errorf("unknown parameter, got: %s", ("t" + shakeMethod + shakeMethodOpt))
+		}
+
+	default:
+		return "", fmt.Errorf("unknown parameter, got: %s", ("t" + shakeMethod + shakeMethodOpt))
+	}
 }
 
 func shakeWord(s string, p string) (string, error) {
@@ -66,8 +171,23 @@ func shakeWord(s string, p string) (string, error) {
 	runes := []rune(strings.TrimSpace(s))
 
 	// buffer variable to work on
-	bufferRunes := make([]rune, len(runes))
-	copy(bufferRunes, runes)
+	var bufferRunes []rune
+
+	//exclude ponctuation
+	ponctuation := ""
+	ponc := 0
+	if unicode.IsPunct(runes[len(runes)-1]) {
+		// buffer variable to work on
+		bufferRunes = make([]rune, len(runes)-1)
+		// separating the ponctuation from the word
+		ponc = 1
+		ponctuation = string(runes[len(runes)-1])
+		copy(bufferRunes, runes[:(len(runes)-1)])
+	} else {
+		// buffer variable to work on
+		bufferRunes = make([]rune, len(runes))
+		copy(bufferRunes, runes)
+	}
 
 	// Check if it contains any spaces
 	if strings.Contains(string(runes), " ") {
@@ -76,13 +196,13 @@ func shakeWord(s string, p string) (string, error) {
 
 	// check word length
 	if p == middleParam {
-		if len(runes) <= 3 {
+		if len(runes) <= (3 + ponc) {
 			return string(runes), nil
 		}
 		// Shake runes between first and last letter
-		bufferRunes = runes[1 : len(runes)-1]
+		bufferRunes = runes[1 : len(runes)-1-ponc]
 	} else {
-		if len(runes) == 1 {
+		if (len(runes) == 1) || (len(runes) == 2 && ponc == 1) {
 			return string(runes), nil
 		}
 	}
@@ -97,9 +217,9 @@ func shakeWord(s string, p string) (string, error) {
 	}
 	if !valid {
 		if p == middleParam {
-			return "", fmt.Errorf("the word cannot be shuffled because it contains identical adjacent letters in the middle")
+			return string(runes), fmt.Errorf("the word cannot be shuffled because it contains identical adjacent letters in the middle")
 		} else {
-			return "", fmt.Errorf("the word cannot be shuffled because it contains identical letters")
+			return string(runes), fmt.Errorf("the word cannot be shuffled because it contains identical letters")
 		}
 	}
 
@@ -132,9 +252,9 @@ func shakeWord(s string, p string) (string, error) {
 	// Rebuild the word
 	var shaked string
 	if p == middleParam {
-		shaked = string(runes[0]) + string(bufferRunes) + string(runes[len(runes)-1])
+		shaked = string(runes[0]) + string(bufferRunes) + string(runes[len(runes)-1-ponc]) + ponctuation
 	} else {
-		shaked = string(bufferRunes)
+		shaked = string(bufferRunes) + ponctuation
 	}
 
 	return shaked, nil
@@ -156,4 +276,52 @@ func reverse(s string) (string, error) {
 	}
 
 	return string(reverseRunes), nil
+}
+
+func reversePlace(w []string) (string, error) {
+	// result variable
+	reverseWords := make([]string, len(w))
+
+	// reverse the word place
+	for i := range w {
+		//if last character is a ponctuation, then move the ponctuation to the other side of the word
+		if unicode.IsPunct(rune(w[i][len(w[i])-1])) {
+			reverseWords[len(w)-i-1] = string(w[i][len(w[i])-1]) + string(w[i][:len(w[i])-1])
+		} else {
+			reverseWords[len(w)-i-1] = w[i]
+		}
+	}
+
+	return strings.Join((reverseWords), " "), nil
+}
+
+func shakePlace(w []string) (string, error) {
+
+	// Copy input to check later the rand.shuffle result
+	shakedPlace := make([]string, len(w))
+	copy(shakedPlace, w)
+
+	// Loop until the shuffle changes the order of the words place
+	for {
+		rand.Seed(uint64(time.Now().UnixNano()))
+		rand.Shuffle(len(shakedPlace), func(i, j int) {
+			shakedPlace[i], shakedPlace[j] = shakedPlace[j], shakedPlace[i]
+		})
+
+		// Check if bufferRunes is different from original
+		changed := false
+		for i := range shakedPlace {
+			if shakedPlace[i] != w[i] {
+				changed = true
+				break
+			}
+		}
+
+		// Break the outer loop if the words place has changed
+		if changed {
+			break
+		}
+	}
+
+	return strings.Join((shakedPlace), " "), nil
 }
--- shakestr_test.go
+++ shakestr_test.go
@@ -12,31 +12,79 @@ func TestShakeforWord(t *testing.T) {
 		errMsg string
 	}{
 		{"", "wm", "", "input value not valid, cannot be empty"},
-		{"Go", "w", "", "parameter length must be at least 2, got: w"},
+		{"Go", "w", "", "parameter length is not valid, got: w"},
+		{"Go", "wwww", "", "parameter length is not valid, got: wwww"},
 		//shuffle middle of the word
 		{"Ace", "wm", "Ace", ""},
+		{"Ace!", "wm", "Ace!", ""},
 		{"Go", "wm", "Go", ""},
+		{"Go?", "wm", "Go?", ""},
 		{"G", "wm", "G", ""},
 		{"Test", "wm", "Tset", ""},
+		{"Test.", "wm", "Tset.", ""},
 		{"Test", "Wm", "Tset", ""},
 		{" Test ", "Wm", "Tset", ""},
-		{"Boot", "Wm", "", "the word cannot be shuffled because it contains identical adjacent letters in the middle"},
+		{" Test, ", "Wm", "Tset,", ""},
+		{"Boot", "Wm", "Boot", "the word cannot be shuffled because it contains identical adjacent letters in the middle"},
 		{"Test", "Zm", "", "unknown parameter: Zm"},
-		{"Test", "wZ", "", "unknown parameter: wZ"},
+		{"Test", "wZ", "", "unknown parameter: wz"},
 		{"Invalid Test", "wm", "", "input must be a single word without spaces, got: Invalid Test"},
 		//shuffle the full word
 		{"G", "wf", "G", ""},
+		{"G!", "wf", "G!", ""},
 		{"Go", "wf", "oG", ""},
+		{"Go?", "wf", "oG?", ""},
 		{"Go", "Wf", "oG", ""},
 		{" Go ", "Wf", "oG", ""},
-		{"oo", "Wf", "", "the word cannot be shuffled because it contains identical letters"},
+		{"oo", "Wf", "oo", "the word cannot be shuffled because it contains identical letters"},
+		{"oo!", "Wf", "oo!", "the word cannot be shuffled because it contains identical letters"},
 		{"Go", "Zf", "", "unknown parameter: Zf"},
-		{"Go", "wZ", "", "unknown parameter: wZ"},
+		{"Go", "wZ", "", "unknown parameter: wz"},
 		{"Invalid Test", "wf", "", "input must be a single word without spaces, got: Invalid Test"},
 		//reverse the full word
 		{"A", "wr", "A", ""},
 		{"Ace", "wr", "ecA", ""},
 		{"Hello You!", "wr", "!uoY olleH", ""},
+	}
+
+	for _, tt := range tests {
+		shaked, err := Shake(tt.s, tt.p)
+
+		if shaked != tt.shaked {
+			t.Errorf("Invalid result for input (%v, %v). expected=%v, got=%v", tt.s, tt.p, tt.shaked, shaked)
+		}
+
+		if tt.errMsg == "" && err != nil {
+			t.Errorf("Expected no error for input (%v, %v) but got: %v", tt.s, tt.p, err)
+		}
+
+		if tt.errMsg != "" && (err == nil || err.Error() != tt.errMsg) {
+			t.Errorf("Invalid error message for input (%v, %v). expected=%v, got=%v", tt.s, tt.p, tt.errMsg, err)
+		}
+
+	}
+}
+
+func TestShakeforText(t *testing.T) {
+	var tests = []struct {
+		s      string
+		p      string
+		shaked string
+		errMsg string
+	}{
+		{"Hello, it’s nice to meet you!", "tzz", "", "unknown parameter, got: tzz"},
+		// reverse mode
+		{"Hello, it’s nice to meet you!", "trz", "", "unknown parameter, got: trz"},
+		{"Hello, it’s nice to meet you!", "tr", "!uoy teem ot ecin s’ti ,olleH", ""},
+		{"Hello, it’s nice to meet you!", "trp", "!you meet to nice it’s ,Hello", ""},
+		//shuffle middle word mode
+		{"Give me your gift.", "tm", "Gvie me yuor gfit.", ""},
+		{"Help me!", "tmp", "me! Hlep", ""},
+		//shuffle full word mode
+		{"He is so up!", "tf", "eH si os pu!", ""},
+		{"To me!", "tfp", "em! oT", ""},
+		//shuffle word place
+		{"Hello, you!", "tp", "you! Hello,", ""},
 	}
 
 	for _, tt := range tests {
